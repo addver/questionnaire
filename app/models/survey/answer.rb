2c3427d066975ed13dae0503cc37a80fade31456
addver <addver@gmail.com>
2020-06-04 14:42:08 +0300

    Update answer.rb

--- app/models/survey/answer.rb
+++ app/models/survey/answer.rb
@@ -9,12 +9,12 @@ class Survey::Answer < ActiveRecord::Base
 
   validates :option_id, :question_id, presence: true
   #validates :predefined_value_id, presence: true, if: proc { |a| a.question && a.question.mandatory? && a.question.predefined_values.count > 0 && ![Survey::OptionsType.text, Survey::OptionsType.large_text].include?(a.option.options_type_id) }
-  validates :option_text, presence: true, if: proc { |a| a.option && (a.question && a.question.mandatory? && a.question.predefined_values.count == 0 && [Survey::OptionsType.text, Survey::OptionsType.multi_choices_with_text, Survey::OptionsType.single_choice_with_text, Survey::OptionsType.large_text].include?(a.option.options_type_id)) }
+  validates :option_text, presence: true, if: proc { |a| a.option && (a.question && a.question.mandatory? && [Survey::OptionsType.text, Survey::OptionsType.multi_choices_with_text, Survey::OptionsType.single_choice_with_text, Survey::OptionsType.large_text].include?(a.option.options_type_id)) }
   validates :option_number, presence: true, if: proc { |a| a.option && (a.question && a.question.mandatory? && [Survey::OptionsType.number, Survey::OptionsType.multi_choices_with_number, Survey::OptionsType.single_choice_with_number].include?(a.option.options_type_id)) }
 
   # rails 3 attr_accessible support
   if Rails::VERSION::MAJOR < 4
-    attr_accessible :option, :attempt, :question, :question_id, :option_id, :predefined_value_id, :attempt_id, :option_text, :option_number
+    attr_accessible :option, :attempt, :question, :question_id, :option_id, :attempt_id, :option_text, :option_number
   end
 
   before_create :characterize_answer
